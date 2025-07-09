683fdbf21d403fe77593a3e0e7f6267b25461fb4
kikugie <stormikiku@gmail.com>
2025-07-09 15:33:54 +0200

    Update to Stonecutter 0.7-beta.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.7-beta.6"
+    id("dev.kikugie.stonecutter") version "0.7-beta.7"
 }
 
 stonecutter {
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("fabric-loom") version "1.11-SNAPSHOT" apply false
     // id("me.modmuss50.mod-publish-plugin") version "0.8.+" apply false // Publishes builds to hosting websites
 }
+
 stonecutter active "1.21.7"
 
 /*
